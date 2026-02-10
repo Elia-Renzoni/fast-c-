ecb66eb3575c7432588230cc38a85506e0f22a5c
Elia Renzoni <elia.renzoni03@gmail.com>
2026-02-10 14:29:41 +0100

    perf: complete files
    Signed-off-by: Elia Renzoni <elia.renzoni03@gmail.com>

--- modularity/Cache.h
+++ modularity/Cache.h
@@ -3,8 +3,16 @@
 
 class Cache {
     public:
-        Cache(int size);
+        Cache(int size) noexcept;
+        ~Cache();
         void setPair(std::string key, std::string value);
         std::string getValue(std::string key);
         void deletePair(std::string key);
+
+    private:
+        std::string *keys;
+        std::string *values;
+
+        int indexPointer;
+        int size;
 };
